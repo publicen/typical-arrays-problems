e4b2a6f3749268de952746506ba20975f04e2d51
publicen <70852922+publicen@users.noreply.github.com>
2020-10-01 21:34:34 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-
 exports.min = function min (array) {
-  return 0;
+if (Array.isArray(array) && (array) != "")
+  		return Math.min.apply(null, array);
+  	else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+if (Array.isArray(array) && (array) != "")
+		return Math.max.apply(null, array);
+  	else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if (Array.isArray(array) && (array) != "")
+		return array.reduce((a, b) => a + b) / array.length;
+  else return 0;
 }
